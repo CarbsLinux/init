3c1b9add8d8dd305106cddec18f1175511aecbe7
Cem Keylan <cem@ckyln.com>
2020-05-16 20:56:30 +0300

    shalt: don't check for root

--- bin/shalt.c
+++ bin/shalt.c
@@ -1,11 +1,8 @@
-#include <unistd.h>
+// shalt -- simple halt utility
 #include <sys/reboot.h>
 
-/* Simple halt utility                                        */
-/* Reboot if the argument is r, Poweroff is the argument is p */
-
 int main (int argc, char *argv[]) {
-    switch ((int)argv[argc < 2 ? 0 : 1][0] + geteuid()) {
+    switch ((int)argv[argc < 2 ? 0 : 1][0]) {
     case 'p': reboot(RB_POWER_OFF); break;
     case 'r': reboot(RB_AUTOBOOT); break;
     default: return 1;
